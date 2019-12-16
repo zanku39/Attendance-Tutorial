9705461850b92acefcb0e00a09fdea17b482a826
kai <mast.mest.boxs@gmail.com>
2019-12-16 04:01:31 +0000

    Finish editing-users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remeber_me] == '1'? remember(user) : forget(user)
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
   end
-  
+
   def destroy
+     # ログイン中の場合のみログアウト処理を実行します。
     log_out if logged_in?
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
